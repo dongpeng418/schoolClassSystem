0565d37d23e4fc39c6c8bfdab5cf053911895ad6
13522669201 <machaohappy_110@163.com>
2019-04-07 09:24:48 +0800

    修改字段名称

--- Class-backend/src/main/java/cn/com/school/classinfo/controller/IndexController.java
+++ Class-backend/src/main/java/cn/com/school/classinfo/controller/IndexController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cn.com.school.classinfo.model.ScStudentInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -25,7 +27,12 @@ public class IndexController {
 
     @ApiOperation(value = "获取avm日志记录，count和list,参数pageSize")
     @GetMapping("/test")
-    public String avmLog() {
-        return "success";
+    public ScStudentInfo avmLog(
+    		@ApiParam(value="用户类别，1老师 2学生",required=true)
+    		String type
+    		) {
+    	ScStudentInfo scStudentInfo = new ScStudentInfo();
+    	scStudentInfo.setStuNo("001");
+        return scStudentInfo;
     }
 }
